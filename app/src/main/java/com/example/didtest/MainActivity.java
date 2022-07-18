0031655e1067b7775b3cffd22f9509bb9f59499f
ongdv <ongdv94@gmail.com>
2022-07-18 14:30:55 +0900

    [UPDATE] Main

---
+++ app/src/main/java/com/example/didtest/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.didtest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
